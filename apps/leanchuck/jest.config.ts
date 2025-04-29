7335c6d5f159446554fa15418d860d85fe3797dd
Austin Miller <Miller0413@yahoo.com>
2025-04-28 20:48:20 -0400

    format

--- apps/leanchuck/jest.config.ts
+++ apps/leanchuck/jest.config.ts
@@ -1,10 +1,13 @@
 export default {
-  displayName: 'leanchuck',
-  preset: '../../jest.preset.js',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/leanchuck',
+	displayName: 'leanchuck',
+	preset: '../../jest.preset.js',
+	testEnvironment: 'node',
+	transform: {
+		'^.+\\.[tj]s$': [
+			'ts-jest',
+			{ tsconfig: '<rootDir>/tsconfig.spec.json' },
+		],
+	},
+	moduleFileExtensions: ['ts', 'js', 'html'],
+	coverageDirectory: '../../coverage/apps/leanchuck',
 };
